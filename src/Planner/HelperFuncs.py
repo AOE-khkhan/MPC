7c3baf9df1a3c7411cd4f4ffb218d31346f4d19a
Apoorv Shrivastava <apoorv.shrivastava@disney.com>
2018-04-09 19:23:26 -0700

    Planner computes desireds for stepwise QPs

--- src/Planner/HelperFuncs.py
+++ src/Planner/HelperFuncs.py
@@ -8,3 +8,24 @@ def getQuadProgCostFunction(J, c, W):
     Q = JtW.dot(J)
     f = -JtW.dot(c)
     return Q, f
+
+def getAngularMomentumInstability(w, I):
+    return np.matmul(skew(w), np.matmul(I, w))
+
+def skew(A):
+    skewA = np.zeros((3,3))
+    skewA[0, 1] = -A[2, 0]
+    skewA[0, 2] = A[1, 0]
+    skewA[1, 0] = A[2, 0]
+    skewA[1, 2] = -A[0, 0]
+    skewA[2, 0] = -A[1, 0]
+    skewA[2, 1] = A[0, 0]
+    return skewA
+
+def getMomentumAboutCoM(x, cop, F):
+    r = cop - x
+    return np.array([[r[1, 0] * F[2, 0] - r[2, 0] * F[1, 0]], [r[2, 0] * F[0, 0] - r[0, 0] * F[2, 0]], [r[0, 0] * F[1, 0] - r[1, 0] * F[0, 0]]])
+
+def getLogDiff(R1, R2):
+    R2inv = np.linalg.inv(R2)
+    return np.matmul(R1, R2inv)
--- src/Planner/SE3Planner.py
+++ src/Planner/SE3Planner.py
@@ -1,7 +1,6 @@
-from builtins import print
 from cvxopt import matrix, log, exp, solvers
+from src.Planner.HelperFuncs import *
 import numpy as np
-from scipy.linalg import expm, logm, det
 
 nodes = 10
 deltaT = 0.1
@@ -9,37 +8,44 @@ T = 2.0
 
 mass = 18.0
 I = np.array([[0.5, 0.0, 0.0], [0.0, 0.34, 0.0], [0.0, 0.0, 0.15]])
+Iinv = np.linalg.inv(I)
 
 nQP = int(T / deltaT)
 x0 = np.array([[0.0],[0.0],[0.5]])
 v0 = np.array([[0.0],[0.0],[0.0]])
 f0 = np.array([[0.0],[0.0],[0.0]])
 R0 = np.array([[1.0, 0.0, 0.0],[0.0, 1.0, 0.0],[0.0, 0.0, 1.0]])
-w0 = np.array([[0.0],[0.0],[0.0]])
+w0 = np.array([[1.0],[2.0],[3.0]])
+cop0 = np.array([[0.0], [0.0], [0.0]])
 
 x = [x0 for i in range(nQP)]
 v = [v0 for i in range(nQP)]
-f = [f0 for i in range(nQP)]
 R = [R0 for i in range(nQP)]
 w = [w0 for i in range(nQP)]
-
+cop = [cop0 for i in range(nQP)]
+f = [f0 for i in range(nQP)]
 
 delx = np.array([[0.0], [0.0], [0.0]])
 delv = np.array([[0.0], [0.0], [0.0]])
-
+delw = np.array([[0.0], [0.0], [0.0]])
+delR = np.array([[0.0], [0.0], [0.0]])
 Jx = np.array([[1.0, 0.0, 0.0, deltaT, 0.0, 0.0, 0.0, 0.0, 0.0],
                [0.0, 1.0, 0.0, 0.0, deltaT, 0.0, 0.0, 0.0, 0.0],
                [0.0, 0.0, 1.0, 0.0, 0.0, deltaT, 0.0, 0.0, 0.0]])
-Jv = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass, 0.0, 0.0],
-              [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass, 0.0],
-              [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass])
-
+Jv = np.array([[0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass, 0.0, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, deltaT / mass]])
 
 for i in range(nQP  - 1):
     im = nQP - i - 1
     errx = x[im] - x[im - 1] - deltaT * v[im - 1]
     errv = v[im] - v[im - 1] - deltaT / mass * f[im - 1]
+    M = getMomentumAboutCoM(x[im - 1], cop[im - 1], f[im - 1])
+    errw = w[im] - w[im - 1] - deltaT * Iinv * M - getAngularMomentumInstability(w[im - 1], I)
+    errR = getLogDiff(R[im], R[im - 1]) - w[im - 1] * deltaT
     objx = delx + errx
     objv = delv + errv
+    objw = delw + errw
+    objR = delR + errR
 
     pass
