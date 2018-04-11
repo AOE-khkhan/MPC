67835759e46b1c15586f6f65bfffcc5c783768ba
Apoorv Shrivastava <apoorv.shrivastava@disney.com>
2018-04-10 19:47:18 -0700

    Storing force result histroy for plotting

--- src/Planner/SE3Planner.py
+++ src/Planner/SE3Planner.py
@@ -6,7 +6,7 @@ import numpy as np
 solvers.options['show_progress'] = False
 
 nodes = 10
-deltaT = 0.1
+deltaT = 0.01
 T = 2.0
 gz = -9.81
 
@@ -16,8 +16,8 @@ Iinv = np.linalg.inv(I)
 
 rmin = np.array([[-0.1], [-0.1], [0.3]])
 rmax = np.array([[0.1], [0.1], [0.5]])
-fmin = np.array([[-10.0], [-10.0], [mass * gz]])
-fmax = np.array([[10.0], [10.0], [mass * -gz]])
+fmin = np.array([[-10.0], [-10.0], [1.001 * mass * gz]])
+fmax = np.array([[10.0], [10.0], [1.001 * mass * -gz]])
 
 nQP = int(T / deltaT)
 x0 = np.array([[0.0], [0.0], [0.40]])
@@ -46,16 +46,21 @@ JR = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, delt
 Jf = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-fd = [np.copy(f0) for i in range(nQP)]
-for i in range(4):
-    fd[i + 7][2] = mass * gz
+fDes = [np.copy(f0) for i in range(nQP)]
+fC = [False for i in range(nQP)]
+count = int(0.4 / deltaT)
+start = int(0.7 / deltaT)
+for i in range(count):
+    fDes[i + start][2] = mass * gz
+    fC[i + start] = True
+    pass
 
 # Initial forward pass
 for i in range(nQP - 1):
     r[i + 1] = r[i] + deltaT * v[i]
-    v[i + 1] = v[i] + deltaT / mass * fd[i]
+    v[i + 1] = v[i] + deltaT / mass * fDes[i]
     R[i + 1] = np.matmul(getRotationMatrix(w[i], deltaT), R[i])
-    M = getMomentumAboutCoM(r[i], cop[i], fd[i])
+    M = getMomentumAboutCoM(r[i], cop[i], fDes[i])
     w[i + 1] = w[i] + deltaT * np.matmul(Iinv, M + getAngularMomentumInstability(w[i], I))
 pass
 
@@ -69,11 +74,10 @@ delv = vf - v[nQP - 1]
 delw = wf - w[nQP - 1]
 delR = getLogDiff(Rf, R[nQP - 1])
 
-Wx = np.identity(3) * 50
-Wv = np.identity(3) * 50
+Wx = np.identity(3) * 1000
+Wv = np.identity(3) * 1000
 WR = np.identity(3) * 1
 Ww = np.identity(3) * 1
-Wf = np.identity(3) * 0.005
 
 Ainx = np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                  [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -88,77 +92,103 @@ Ainf = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.
                  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
 nIterations = 2
-fz = [0 for i in range(nIterations)]
-
+fHist = [[np.zeros((3,1)) for j in range(nQP)] for i in range(nIterations)]
 for j in range(nIterations):
     # Backward pass
-    for i in range(nQP - 1):
-        im = nQP - i - 1
-        errx = r[im] - r[im - 1] - deltaT * v[im - 1]
-        errv = v[im] - v[im - 1] - deltaT / mass * f[im - 1]
-        M = getMomentumAboutCoM(r[im - 1], cop[im - 1], f[im - 1])
-        errw = w[im] - w[im - 1] - deltaT * np.matmul(Iinv, M - getAngularMomentumInstability(w[im - 1], I))
-        errR = getLogDiff(R[im], R[im - 1]) - w[im - 1] * deltaT
-        objx = delx + errx
-        objv = delv + errv
-        objw = delw + errw
-        objR = delR + errR
-        objf = np.array([[0.0], [0.0], [0.0]])
-        Ixx = I.item((0, 0))
-        Iyy = I.item((1, 1))
-        Izz = I.item((2, 2))
-        Fx = f[im - 1].item(0)
-        Fy = f[im - 1].item(1)
-        Fz = f[im - 1].item(2)
-        x = r[im - 1].item(0)
-        y = r[im - 1].item(1)
-        z = r[im - 1].item(2)
-        xcop = cop[im - 1].item(0)
-        ycop = cop[im - 1].item(1)
-        zcop = cop[im - 1].item(2)
-        Jw = np.array([[0.0, -Fz, Fy, 0.0, 0.0, 0.0, 0.0, z - zcop, ycop - y, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Fz, -Fy],
-                       [Fz, 0.0, -Fx, 0.0, 0.0, 0.0, zcop - z, 0.0, x - xcop, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, -Fz, 0.0, Fx],
-                       [-Fy, 0.0, Fx, 0.0, 0.0, 0.0, y - ycop, 0.0, xcop - x, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Fy, 0.0, -Fx]]) * deltaT
-        Jw[0, :] *= 1.0/Ixx
-        Jw[1, :] *= 1.0/Iyy
-        Jw[2, :] *= 1.0/Izz
-        Q = np.identity(18) * 0.000001
-        F = np.zeros((18, 1))
-        Qd, fd = getQuadProgCostFunction(Jf, objf, Wf)
-        Q +=  Qd
-        F += fd
-        Qd, fd = getQuadProgCostFunction(Jx, objx, Wx)
-        Q +=  Qd
-        F += fd
-        binx = np.vstack((rmax - r[im], -rmin + r[im]))
-        Qd, fd = getQuadProgCostFunction(Jv, objv, Wv)
-        Q += Qd
-        F += fd
-        Qd, fd = getQuadProgCostFunction(Jw, objw, Ww)
-        Q += Qd
-        F += fd
-        Qd, fd = getQuadProgCostFunction(JR, objR, WR)
-        Q += Qd
-        F += fd
-        binf = np.vstack((fmax - f[im], -fmin + f[im]))
-        Ain = np.vstack((Ainx, Ainf))
-        Bin = np.vstack((binx, binf))
-        P = matrix(Q, tc='d')
-        q = matrix(F, tc='d')
-        G = matrix(Ain, tc='d')
-        h = matrix(Bin, tc='d')
+    delx = xf - r[nQP - 1]
+    delv = vf - v[nQP - 1]
+    try:
+        for i in range(nQP - 1):
+            im = nQP - i - 1
+            errx = r[im] - r[im - 1] - deltaT * v[im - 1]
+            errv = v[im] - v[im - 1] - deltaT / mass * f[im - 1]
+            M = getMomentumAboutCoM(r[im - 1], cop[im - 1], f[im - 1])
+            errw = w[im] - w[im - 1] - deltaT * np.matmul(Iinv, M - getAngularMomentumInstability(w[im - 1], I))
+            errR = getLogDiff(R[im], R[im - 1]) - w[im - 1] * deltaT
+            objx = 0.75 * (delx + errx)
+            objv = 0.75 * (delv + errv)
+            objw = delw + errw
+            objR = delR + errR
+            objf = f[im] - f[im - 1]
+            Ixx = I.item((0, 0))
+            Iyy = I.item((1, 1))
+            Izz = I.item((2, 2))
+            Fx = f[im - 1].item(0)
+            Fy = f[im - 1].item(1)
+            Fz = f[im - 1].item(2)
+            x = r[im - 1].item(0)
+            y = r[im - 1].item(1)
+            z = r[im - 1].item(2)
+            xcop = cop[im - 1].item(0)
+            ycop = cop[im - 1].item(1)
+            zcop = cop[im - 1].item(2)
+            Jw = np.array([[0.0, -Fz, Fy, 0.0, 0.0, 0.0, 0.0, z - zcop, ycop - y, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Fz, -Fy],
+                           [Fz, 0.0, -Fx, 0.0, 0.0, 0.0, zcop - z, 0.0, x - xcop, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, -Fz, 0.0, Fx],
+                           [-Fy, 0.0, Fx, 0.0, 0.0, 0.0, y - ycop, 0.0, xcop - x, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Fy, 0.0, -Fx]]) * deltaT
+            Jw[0, :] *= 1.0/Ixx
+            Jw[1, :] *= 1.0/Iyy
+            Jw[2, :] *= 1.0/Izz
+            Q = np.identity(18) * 0.001
+            F = np.zeros((18, 1))
+            if(fC[im - 1]):
+                Aeqf = Jf
+                Beqf = fDes[im - 1] - f[im - 1]
+                Wf = np.identity(3) * 2
+            else:
+                Wf = np.identity(3) * 2
+                Aeqf = []
+                Beqf = []
+            Qd, fd = getQuadProgCostFunction(Jf, objf, Wf)
+            Q +=  Qd
+            F += fd
+            Qd, fd = getQuadProgCostFunction(Jx, objx, Wx)
+            Q +=  Qd
+            F += fd
+            binx = np.vstack((rmax - r[im], -rmin + r[im]))
+            Qd, fd = getQuadProgCostFunction(Jv, objv, Wv)
+            Q += Qd
+            F += fd
+            Qd, fd = getQuadProgCostFunction(Jw, objw, Ww)
+            Q += Qd
+            F += fd
+            Qd, fd = getQuadProgCostFunction(JR, objR, WR)
+            Q += Qd
+            F += fd
+            binf = np.vstack((fmax - f[im], -fmin + f[im]))
+            Ain = np.vstack((Ainx, Ainf))
+            Bin = np.vstack((binx, binf))
 
-        soln = solvers.qp(P, q, G, h)
-        dOpt = soln['x']
-        delx = np.array(dOpt[0:3])
-        delv = np.array(dOpt[3:6])
-        delf = np.array(dOpt[6:9])
-        delR = np.array(dOpt[9:12])
-        delw = np.array(dOpt[12:15])
-        delCoP = np.array(dOpt[15:18])
-        f[im - 1] += delf
-        cop[im - 1] += delCoP
+            P = matrix(Q, tc='d')
+            q = matrix(F, tc='d')
+            G = matrix(Ain, tc='d')
+            h = matrix(Bin, tc='d')
+            if(fC[im - 1]):
+                A = matrix(Aeqf, tc='d')
+                b = matrix(Beqf, tc='d')
+                soln = solvers.qp(P, q, G, h, A, b)
+            else:
+                soln = solvers.qp(P, q, G, h)
+
+            dOpt = soln['x']
+            delx = np.array(dOpt[0:3])
+            delv = np.array(dOpt[3:6])
+            delf = np.array(dOpt[6:9])
+            delR = np.array(dOpt[9:12])
+            delw = np.array(dOpt[12:15])
+            delCoP = np.array(dOpt[15:18])
+            f[im - 1] += delf
+            cop[im - 1] += delCoP
+            pass
+    except Exception as e:
+        print("Qp broke after " + str(j) + " iterations. ")
+        for i in range(nQP - 1):
+            r[i + 1] = r[i] + deltaT * v[i]
+            v[i + 1] = v[i] + deltaT / mass * f[i]
+            R[i + 1] = np.matmul(getRotationMatrix(w[i], deltaT), R[i])
+            M = getMomentumAboutCoM(r[i], cop[i], f[i])
+            w[i + 1] = w[i] + deltaT * np.matmul(Iinv, M + getAngularMomentumInstability(w[i], I))
         pass
+        break
 
     # Forward pass
     for i in range(nQP - 1):
@@ -168,10 +198,14 @@ for j in range(nIterations):
         M = getMomentumAboutCoM(r[i], cop[i], f[i])
         w[i + 1] = w[i] + deltaT * np.matmul(Iinv, M + getAngularMomentumInstability(w[i], I))
     pass
-    fz[j] = f[nQP - 1].item(2)
+    for k in range(nQP):
+        fHist[j][k][0] = f[k].item(0)
+        fHist[j][k][1] = f[k].item(1)
+        fHist[j][k][2] = f[k].item(2)
+    pass
 pass
 
-tData = [0.1 * i for i in range(nQP)]
+tData = [deltaT * i for i in range(nQP)]
 pxData = [r[i].item(0) for i in range(nQP)]
 pyData= [r[i].item(1) for i in range(nQP)]
 pzData = [r[i].item(2) for i in range(nQP)]
@@ -237,6 +271,10 @@ fig.show()
 
 fig = plt.figure()
 conPlot = fig.add_subplot(111)
-n = [i for i in range(nIterations)]
-conPlot.plot(n, fz)
+n = [i for i in range(nQP)]
+for i in range(nIterations):
+    fz = [fHist[i][j].item(2) for j in range(nQP)]
+    conPlot.plot(n, fz, label="iter" + str(i))
+pass
+conPlot.legend()
 fig.show()
